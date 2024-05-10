078903af024d4703dca05cf0775e2239518582e4
Arthur Oliveira <arthurcabraloliveira@hotmail.com>
2024-05-10 01:06:12 -0300

    message included on RequestRescue return

--- RescueRS/Presenter/Controllers/App/V1/Rescue/DTOs/ResponseDTO.cs
+++
@@ -1,6 +0,0 @@
-namespace ResgateRS.Presenter.Controllers.App.V1.DTOs;
-public class ResponseDTO
-{
-    public string Message { get; set; } = null!;
-    public string DebugMessage { get; set; } = null!;
-}
